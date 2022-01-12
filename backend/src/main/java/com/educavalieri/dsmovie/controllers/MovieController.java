a22396c9e75f004be05e7348e9d9311c145d1cd7
educavalieri <eduardocavalieri@hotmail.com>
2022-01-12 17:21:10 -0300

    Find ID movie

--- backend/src/main/java/com/educavalieri/dsmovie/controllers/MovieController.java
+++ backend/src/main/java/com/educavalieri/dsmovie/controllers/MovieController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
@@ -21,5 +22,11 @@ public class MovieController {
     public Page<MovieDTO> findAll(Pageable pageable){
         return service.findAll(pageable);
     }
+
+    @GetMapping(value = "/{id}")
+    public MovieDTO findById(@PathVariable("id") Long id){
+        return service.findById(id);
+    }
+
 
 }
--- backend/src/main/java/com/educavalieri/dsmovie/services/MovieService.java
+++ backend/src/main/java/com/educavalieri/dsmovie/services/MovieService.java
@@ -25,4 +25,10 @@ public class MovieService {
         return page;
     }
 
+    public MovieDTO findById(Long id){
+        Movie result = repository.findById(id).get();
+        MovieDTO dto = new MovieDTO(result);
+        return dto;
+    }
+
 }
